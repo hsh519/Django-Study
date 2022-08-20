3adab6179fc742db205d792f9cc7502342f6e376
hsh_519 <suk000519@naver.com>
2022-08-21 08:28:02 +0900

    DRF_practice_notionboard(model update)

--- DRF_practice_F/myboard/posts/models.py
+++ DRF_practice_F/myboard/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE, blank=True)
     title = models.CharField(max_length=128)
     category = models.CharField(max_length=128)
     body = models.TextField()
-    image = models.ImageField(upload='post/', default='default.png')
-    like = models.ManyToManyField('users.User')
+    image = models.ImageField(upload_to='post/', default='default.png')
+    like = models.ManyToManyField(User, related_name='like_posts', blank=True)
     published_date = models.DateTimeField(default=timezone.now)
